0cc87a5790c156c2a9b09b3677cc3865ada2e10b
LarsVandenberghe <lars.vandenberghe@hotmail.be>
2026-05-18 17:53:43 +0200

    Add springboot application

---
+++ HoneyQBE/src/test/java/be/honeyq/HoneyQBE/HoneyQbeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.honeyq.HoneyQBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyQbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
